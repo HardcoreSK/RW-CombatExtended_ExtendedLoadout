5dfa3f9fca70c567bf1c01fae955fd71efd16f53
TheLoneTec <kaceysherrard@hotmail.com>
2025-07-19 09:59:01 +0100

    Consolidate changes

--- Source/Loadout/Patches/CustomLoadouts/Utility_Loadouts_GetLoadout_Patch.cs
+++ Source/Loadout/Patches/CustomLoadouts/Utility_Loadouts_GetLoadout_Patch.cs
@@ -13,22 +13,7 @@ public static class Utility_Loadouts_GetLoadout_Patch
     public static bool GetLoadout(Pawn pawn, ref Loadout __result)
     {
         // original => return Loadout or LoadoutManager.DefaultLoadout
-        __result = LoadoutMulti_Manager.GetLoadout(pawn);
-        return false;
-    }
-}
-
-[HarmonyPatch(typeof(Utility_Loadouts))]
-public static class Utility_Loadouts_GetLoadoutId_Patch
-{
-    static bool Prepare() => ExtendedLoadoutMod.Instance.useMultiLoadouts;
-
-    [HarmonyPatch(nameof(Utility_Loadouts.GetLoadoutId))]
-    [HarmonyPrefix]
-    public static bool GetLoadoutId(Pawn pawn, ref int __result)
-    {
-        // original => return Loadout or LoadoutManager.DefaultLoadout
-        __result = LoadoutMulti_Manager.GetLoadout(pawn, false)!.UniqueID;
+        __result = LoadoutMulti_Manager.GetLoadout(pawn, true)!;
         return false;
     }
 }
