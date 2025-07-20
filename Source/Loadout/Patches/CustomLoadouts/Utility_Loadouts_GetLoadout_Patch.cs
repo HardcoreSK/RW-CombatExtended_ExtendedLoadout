897a9dd3e167f08f31718819955443701f00e203
TheLoneTec <kaceysherrard@hotmail.com>
2025-07-20 19:05:37 +0100

    One more patch, any more reports and ill rethink it

--- Source/Loadout/Patches/CustomLoadouts/Utility_Loadouts_GetLoadout_Patch.cs
+++ Source/Loadout/Patches/CustomLoadouts/Utility_Loadouts_GetLoadout_Patch.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using System.Collections.Generic;
+using System;
 using Verse;
 
 namespace CombatExtended.ExtendedLoadout;
@@ -29,6 +31,40 @@ public static class Utility_Loadouts_GetLoadoutId_Patch
     {
         // original => return Loadout or LoadoutManager.DefaultLoadout
         __result = LoadoutMulti_Manager.GetLoadout(pawn, false)!.UniqueID;
+        return false;
+    }
+}
+
+[HarmonyPatch(typeof(Utility_HoldTracker))]
+public static class Utility_HoldTracker_Notify_HoldTrackerItem_Patch
+{
+    static bool Prepare() => ExtendedLoadoutMod.Instance.useMultiLoadouts;
+
+    [HarmonyPatch(nameof(Utility_HoldTracker.Notify_HoldTrackerItem))]
+    [HarmonyPrefix]
+    public static bool Notify_HoldTrackerItem(Pawn pawn, Thing item, int count)
+    {
+        if (LoadoutMulti_Manager.GetLoadout(pawn, false)!.defaultLoadout)
+            return false;
+        List<HoldRecord> holdRecordList = LoadoutManager.GetHoldRecords(pawn);
+        if (holdRecordList == null)
+        {
+            holdRecordList = new List<HoldRecord>();
+            LoadoutManager.AddHoldRecords(pawn, holdRecordList);
+        }
+            HoldRecord holdRecord1 = holdRecordList.FirstOrDefault<HoldRecord>((Predicate<HoldRecord>) (hr => hr.thingDef == item.def));
+        if (holdRecord1 != null)
+        {
+        if (holdRecord1.pickedUp)
+            holdRecord1.count = Utility_HoldTracker.GetMagazineAwareStackCount(pawn, holdRecord1.thingDef) + count;
+        else
+            holdRecord1.count += count;
+        }
+        else
+        {
+            HoldRecord holdRecord2 = new HoldRecord(item.def, count);
+            holdRecordList.Add(holdRecord2);
+        }
         return false;
     }
 }
